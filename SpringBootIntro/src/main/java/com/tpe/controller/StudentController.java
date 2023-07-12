de731507d56c417d2921b18644e90c9cdee1d944
Mucahit <mcht.almc@gmail.com>
2023-07-12 15:50:44 +0300

    Spring Boot
    Simple project with Spring Boot

---
+++ SpringBootIntro/src/main/java/com/tpe/controller/StudentController.java
@@ -0,0 +1,198 @@
+package com.tpe.controller;
+
+
+import com.tpe.domain.Student;
+import com.tpe.dto.StudentDTO;
+import com.tpe.service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//@Controller
+@RestController
+@RequestMapping("/students")//http://localhost:8080/students
+public class StudentController {
+
+    Logger logger= LoggerFactory.getLogger(StudentController.class);
+
+    @Autowired
+    private StudentService studentService;
+
+    //spring boot u selamlama:)
+    //http://localhost:8080/students/greet+GET
+    @GetMapping("/greet")
+    public String greet(){
+        return "Hello Spring Boot";
+    }
+
+    //1-tüm studentları listeleyelim:READ
+    //http://localhost:8080/students + GET
+    @PreAuthorize("hasRole('ADMIN')")//sadece ROLE_ADMIN bu isteği yapabilir.
+    @GetMapping
+    public ResponseEntity<List<Student>> listAllStudents(){
+        List<Student> studentList=studentService.getAllStudent();
+        //return new ResponseEntity<>(studentList, HttpStatus.OK);//200
+        return ResponseEntity.ok(studentList);//200
+    }
+    //response:body(data)+HTTP status code
+    //ResponseEntity: response bodysi ile birlikte HTTP status code nu göndermemizi sağlar.
+    //ResponseEntity.ok() metodu HTTP status olarak OK yada 200 dönmek için bir kısayoldur.
+
+    //3-yeni bir student CREATE etme
+    //http://localhost:8080/students + POST + RequestBody(JSON)
+    @PostMapping
+    public ResponseEntity<Map<String,String>> createStudent(@Valid @RequestBody Student student){
+
+        studentService.saveStudent(student);
+
+        Map<String,String> response=new HashMap<>();
+        response.put("message","Student is created successfully");
+        response.put("status","success");
+        return new ResponseEntity<>(response,HttpStatus.CREATED);//201
+    }
+//@RequestBody:HTTP requestin bodysindeki JSON formatındaki bilgiyi student objesine mapler.
+// (Entity obje<->JSON)-->Jackson
+
+    //5-belirli bir id ile studentı görüntüleyelim + Request Param
+    //http://localhost:8080/students/query?id=1 + GET
+    @GetMapping("/query")
+    public ResponseEntity<Student> getStudent(@RequestParam("id") Long id){
+        Student student=studentService.getStudentById(id);
+        return new ResponseEntity<>(student,HttpStatus.OK);//200
+    }
+
+
+    //5(2.yöntem)-belirli bir id ile studentı görüntüleyelim +Path Param
+    //http://localhost:8080/students/1 + GET
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable("id") Long id){
+        Student student=studentService.getStudentById(id);
+        return new ResponseEntity<>(student,HttpStatus.OK);//200
+    }
+
+    //clienttan bilgi almak için:JSON formatında request body
+    //                           :request param query?id=1
+    //                           :path param /1
+
+    //7-belirli bir id ile studentı silelim +Path Param
+    //http://localhost:8080/students/1 + DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String,String>> deleteStudent(@PathVariable Long id){
+
+        studentService.deleteStudent(id);
+
+        Map<String,String> response=new HashMap<>();
+        response.put("message","Student is deleted successfully..");
+        response.put("status","success");
+        return ResponseEntity.ok(response);//200
+
+    }
+
+    //9-belirli bir id ile studentı update edelim.(name,lastName,grade,email,phoneNumber)
+    //http://localhost:8080/students/1 + UPDATE + JSON
+    @PutMapping("/{id}")
+    public ResponseEntity<Map<String,String>> updateStudent(@PathVariable("id") Long id,
+                                                            @Valid @RequestBody StudentDTO studentDTO){
+
+        studentService.updateStudent(id,studentDTO);
+
+        Map<String,String> response=new HashMap<>();
+        response.put("message","Student is updated successfully..");
+        response.put("status","success");
+        return ResponseEntity.ok(response);//200
+    }
+
+    //11-pagination-sayfalandırma
+    //tüm kayıtları page page listeleyelim
+    //http://localhost:8080/students/page?
+    //                               page=1&
+    //                               size=10&
+    //                               sort=name&
+    //                               direction=DESC + GET
+    @GetMapping("/page")
+    public ResponseEntity<Page<Student>> getAllStudentByPage
+    (@RequestParam(value = "page",required = false,defaultValue = "0") int page,//hangi page gösterilsin ilk değer 0
+    @RequestParam(value = "size",required = false,defaultValue = "2") int size,//kaç kayıt
+    @RequestParam("sort") String prop,//hangi fielda göre
+    @RequestParam("direction") Sort.Direction direction)//sıralama yönü
+    {
+        Pageable pageable= PageRequest.of(page,size,Sort.by(direction,prop));
+        Page<Student> studentsByPage=studentService.getAllStudentPaging(pageable);
+        return new ResponseEntity<>(studentsByPage,HttpStatus.OK);
+    }
+
+    //page,size,sort,direction parametrelerinin girilmesini opsiyonel yapabiliriz.(required=false)
+    //parametrelerin girilmesi opsiyonel olduğunda default value vermeliyiz.(defaultValue = "0")
+
+
+
+
+    //13-lastName ile studentları listeleyelim. **SIRA SİZDE**
+    ////http://localhost:8080/students/querylastname?lastName=Bey
+    @GetMapping("/querylastname")
+    public ResponseEntity<List<Student>> getAllStudentsByLastName(@RequestParam("lastName") String lastname){
+
+        List<Student> studentList=studentService.getAllStudentByLastName(lastname);
+
+        return ResponseEntity.ok(studentList);
+    }
+
+     //15-grade ile studentları listeleyelim. **ÖDEV**
+     //http://localhost:8080/students/grade/99
+    @GetMapping("/grade/{grade}")
+    public ResponseEntity<List<Student>> getAllStudentByGrade(@PathVariable("grade") Integer grade){
+
+        List<Student> studentList=studentService.getAllStudentByGrade(grade);
+        return ResponseEntity.ok(studentList);
+    }
+
+    //17-idsi verilen studentın görüntüleme requestine response olarak DTO dönelim
+    //http://localhost:8080/students/dto/1 + GET
+    @GetMapping("/dto/{id}")
+    public ResponseEntity<StudentDTO> getStudentDtoById(@PathVariable("id") Long id){
+       StudentDTO studentDTO= studentService.getStudentDtoById(id);
+
+       logger.warn("--------serviceden StudentDTO objesi alındı: "+studentDTO.getName());
+
+       return ResponseEntity.ok(studentDTO);
+    }
+
+    //19-
+    @GetMapping("/welcome")
+    public String welcome(HttpServletRequest request){
+
+        logger.info("welcome mesajı  {}",request.getServletPath());
+
+        return "WELCOME:)";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ SpringBootIntro/src/main/java/com/tpe/service/StudentService.java
@@ -0,0 +1,121 @@
+package com.tpe.service;
+
+import com.tpe.domain.Student;
+import com.tpe.dto.StudentDTO;
+import com.tpe.exception.ConflictException;
+import com.tpe.exception.ResourceNotFoundException;
+import com.tpe.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    //2-
+    public List<Student> getAllStudent() {
+        //List<Student> students=studentRepository.findAll();//select * from student;
+        //return students;
+        return studentRepository.findAll();
+    }
+
+    //4-
+    public void saveStudent(Student student) {
+        //student daha önce kaydedilmiş mi?-->aynı emaile sahip student var mı?
+
+        if(studentRepository.existsByEmail(student.getEmail())){
+            throw new ConflictException("Email already exists!");
+        }
+        studentRepository.save(student);
+    }
+
+   //6-
+    public Student getStudentById(Long id) {
+        Student student=studentRepository.findById(id).orElseThrow(()->
+                new ResourceNotFoundException("Student not found by id: "+ id));
+        return student;
+    }
+
+    //8-
+    public void deleteStudent(Long id) {
+        //bu id ye sahip bir student var mı?
+        Student foundStudent=getStudentById(id);
+        studentRepository.delete(foundStudent);
+    }
+
+    //10-
+    public void updateStudent(Long id, StudentDTO studentDTO) {
+        //gelen id ile öğrenci var mı? varsa bulalım
+        Student foundStudent=getStudentById(id);
+
+        //studentDTO.getEmail() zaten daha önceden DB de varsa??
+        boolean existsEmail=studentRepository.existsByEmail(studentDTO.getEmail());
+
+        //existsEmail true ise bu email başka bir studentın olabilir, studentın kendi emaili olabilir??
+        // id:3 student email:a@email.com
+        //dto                student
+        //b@email.com        id:4 b@email.com->existsEmail:true--->exception -senaryo 1
+        //c@email.com        DB de yok-------->existsEmail:false--update:OK -senaryo 2
+        //a@email.com        id:3 a@email.com ->existsEmail:true--update:OK -senaryo 3
+
+
+        if(existsEmail && !foundStudent.getEmail().equals(studentDTO.getEmail())){
+            throw new ConflictException("Email already exists!!!");
+        }
+
+        foundStudent.setName(studentDTO.getName());
+        foundStudent.setLastName(studentDTO.getLastName());
+        foundStudent.setGrade(studentDTO.getGrade());
+        foundStudent.setPhoneNumber(studentDTO.getPhoneNumber());
+        foundStudent.setEmail(studentDTO.getEmail());
+
+        studentRepository.save(foundStudent);//saveOrUpdate gibi işlem yapar
+
+    }
+
+    //12-
+    public Page<Student> getAllStudentPaging(Pageable pageable) {
+
+       return studentRepository.findAll(pageable);
+
+    }
+
+    //14-
+    public List<Student> getAllStudentByLastName(String lastname) {
+        return studentRepository.findAllByLastName(lastname);//select * from student where lastName=''
+    }
+
+    //16-
+    public List<Student> getAllStudentByGrade(Integer grade) {
+       // return studentRepository.findAllByGrade(grade);
+        return studentRepository.findAllGradeEquals(grade);
+    }
+
+
+    //18-
+//    public StudentDTO getStudentDtoById(Long id) {
+//        Student student=getStudentById(id);
+//
+//        //parametre olarak student objesinin kendisini verdiğimizde DTO oluşturan bir constructor
+//        StudentDTO studentDTO=new StudentDTO(student);
+//        return studentDTO;
+//    }
+
+
+    //studentı dto ya mapleme işlemini JPQL ile yapalım
+    public StudentDTO getStudentDtoById(Long id) {
+        StudentDTO studentDTO=studentRepository.findStudentDtoById(id).orElseThrow(()->
+                new ResourceNotFoundException("Student not found by id: "+id));
+        return studentDTO;
+    }
+
+
+
+
+}
